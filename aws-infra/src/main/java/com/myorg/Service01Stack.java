bb46b04256271e6464af5bbebdaedcfcd963512d
Rodrigo Lúcio <luciodigo@gmail.com>
2023-03-20 21:50:14 -0300

    Criado a taskImageOptions

--- aws-infra/src/main/java/com/myorg/Service01Stack.java
+++ aws-infra/src/main/java/com/myorg/Service01Stack.java
@@ -1,9 +1,15 @@
 package com.myorg;
 
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 public class Service01Stack extends Stack {
@@ -19,9 +25,23 @@ public class Service01Stack extends Stack {
                 .serviceName("service-01")
                 .cluster(cluster)
                 .cpu(512)
+                .memoryLimitMiB(1024)
                 .desiredCount(2)
                 .listenerPort(8080)
-                .memoryLimitMiB(1024)
+                .assignPublicIp(true)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_project01")
+                                .image(ContainerImage.fromRegistry("rodrigolucio/service01"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
+                                                        .logGroupName("Service01")
+                                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                                        .build())
+                                        .streamPrefix("Service01")
+                                        .build()))
+                                .build())
                 .build();
     }
 }
