6eb21874a08175d0b985399e9dc73d653298084a
Rodrigo Lúcio <luciodigo@gmail.com>
2023-04-05 20:23:29 -0300

    Create ProductController.java

---
+++ service01/src/main/java/br/com/lucio/service01/controller/ProductController.java
@@ -0,0 +1,66 @@
+package br.com.lucio.service01.controller;
+
+import br.com.lucio.service01.model.Product;
+import br.com.lucio.service01.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/products")
+public class ProductController {
+
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product, UriComponentsBuilder uriBuilder) {
+        Product productCreated = productRepository.save(product);
+        URI uri = uriBuilder.path("/api/products/{id}").buildAndExpand(productCreated.getId()).toUri();
+        return ResponseEntity.created(uri).body(productCreated);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Product> update(@RequestBody Product product, @PathVariable Long id) {
+        if (productRepository.existsById(id)) {
+            product.setId(id);
+            Product productUpdated = productRepository.save(product);
+            return ResponseEntity.ok(productUpdated);
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> delete(@PathVariable Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
