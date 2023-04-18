7e817b602ba1876d7dbd227ef474bf0aa4a8af10
Rodrigo Lúcio <luciodigo@gmail.com>
2023-04-17 22:29:40 -0300

    Create ProductDTO.java

---
+++ service01/src/main/java/br/com/lucio/service01/model/ProductDTO.java
@@ -0,0 +1,32 @@
+package br.com.lucio.service01.model;
+
+public class ProductDTO {
+
+    private Long id;
+    private String code;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
