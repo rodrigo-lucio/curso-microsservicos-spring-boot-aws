44af82184a62c0134f36f9402febfa50133683ab
Rodrigo Lúcio <luciodigo@gmail.com>
2023-05-09 22:50:27 -0300

    Salvando os eventos vindos do SQS do produto no DynamoDB

--- service02/src/main/java/br/com/lucio/service02/service/ProductEventConsumer.java
+++ service02/src/main/java/br/com/lucio/service02/service/ProductEventConsumer.java
@@ -1,8 +1,8 @@
 package br.com.lucio.service02.service;
 
-import br.com.lucio.service02.model.CrudEventDTO;
-import br.com.lucio.service02.model.ProductDTO;
-import br.com.lucio.service02.model.SnsMessage;
+import br.com.lucio.service02.model.*;
+import br.com.lucio.service02.model.dynamo.ProductEvent;
+import br.com.lucio.service02.repository.ProductEventRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 @Service
 public class ProductEventConsumer {
@@ -20,8 +22,11 @@ public class ProductEventConsumer {
 
     private ObjectMapper objectMapper;
 
-    public ProductEventConsumer(ObjectMapper objectMapper) {
+    private ProductEventRepository productEventRepository;
+
+    public ProductEventConsumer(ObjectMapper objectMapper, ProductEventRepository productEventRepository) {
         this.objectMapper = objectMapper;
+        this.productEventRepository = productEventRepository;
     }
 
     @JmsListener(destination = "${aws.sqs.queue.product.events.name}")
@@ -29,8 +34,27 @@ public class ProductEventConsumer {
         SnsMessage snsMessage = objectMapper.readValue(textMessage.getText(), SnsMessage.class);
         CrudEventDTO crudEventDTO = objectMapper.readValue(snsMessage.getMessage(), CrudEventDTO.class);
         ProductDTO productDTO = objectMapper.readValue(crudEventDTO.getData(), ProductDTO.class);
+
+        ProductEvent productEvent = buildProductEvent(crudEventDTO, productDTO);
+        productEventRepository.save(productEvent);
+
         log.info("Product event received - Event: {} - ProductId: {} - MessageId: {}",
                 crudEventDTO.getEventType(), productDTO.getId(), snsMessage.getMessageId());
+    }
+
+    private ProductEvent buildProductEvent(CrudEventDTO crudEventDTO, ProductDTO productDTO) {
+
+        ProductEvent productEvent = new ProductEvent();
+        long timestamp = Instant.now().toEpochMilli();
+        productEvent.setPk(productDTO.getCode());
+        productEvent.setSk(crudEventDTO.getEventType() + "_" + timestamp);
+        productEvent.setEventType(crudEventDTO.getEventType());
+        productEvent.setProductId(productDTO.getId());
+        productEvent.setUsername(productDTO.getUsername());
+        productEvent.setTimestamp(timestamp);
+        productEvent.setTtl(Instant.now().plus(Duration.ofMinutes(10)).getEpochSecond()); //Em aprox 10 minutos o dynamo vai remover esse registro
+        log.info("Product event builded: {}", productEvent.toString());
+        return productEvent;
     }
 
 }
