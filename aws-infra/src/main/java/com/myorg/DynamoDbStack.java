8a5a2bba7a8d6e000d791d28e3c0098d38d07865
Rodrigo Lúcio <luciodigo@gmail.com>
2023-05-15 21:45:38 -0300

    Criando auto scale da tabela do dynamo

--- aws-infra/src/main/java/com/myorg/DynamoDbStack.java
+++ aws-infra/src/main/java/com/myorg/DynamoDbStack.java
@@ -1,12 +1,10 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.dynamodb.Attribute;
-import software.amazon.awscdk.services.dynamodb.AttributeType;
-import software.amazon.awscdk.services.dynamodb.BillingMode;
-import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.*;
 import software.constructs.Construct;
 
 public class DynamoDbStack extends Stack {
@@ -35,6 +33,28 @@ public class DynamoDbStack extends Stack {
                 .timeToLiveAttribute("ttl") //tempo em que fica armazenado o registro
                 .removalPolicy(RemovalPolicy.DESTROY) //Destroi a tabela quando a stack for removida
                 .build();
+
+        //Auto scale da tabela do Dynamo
+        producEventsTable.autoScaleReadCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
+        producEventsTable.autoScaleWriteCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
     }
 
     public Table getProducEventsTable() {
